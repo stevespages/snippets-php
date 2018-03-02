880ccb7e4731de3b07793650b601e3fcb1088d0c
Steve Greig <greigsteve@gmail.com>
2018-03-02 21:23:52 +0000

    Commit before trying to refactor to make logic easier to follow

--- index.php
+++ index.php
@@ -88,6 +88,25 @@ function home($pdo)
 										),
 						"validate" => ""
 						)
+/*			"image_1" => array 	(
+						"name" => "image_1",
+						"required" => "",
+						"value" => "",
+						"error_mssg" => "",
+						"form_label" => "Image 1",
+						"type" => "file",
+						"validate" => ""
+						),
+			"image_2" => array 	(
+						"name" => "image_2",
+						"required" => "",
+						"value" => "",
+						"error_mssg" => "",
+						"form_label" => "Image 2",
+						"type" => "file",
+						"validate" => ""
+						)
+*/
     );
     $action = "index.php";
     If (isset($_GET['id'])) {
